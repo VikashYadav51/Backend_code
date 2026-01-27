50930b9a5f9fabbaf2fa3f7e90eca0edafab1ee3
Vikash <vikashyadav5131@gmail.com>
2026-01-27 12:27:58 +0530

    restructure the app.js files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
 import express from 'express';
 
+import cookieParser from 'cookie-parser';
+
+import cors from 'cors';
+
 const app = express();
+
+const coreOptions = {
+    origin : process.env.CORS_ORIGIN,
+    optionsSuccessStatus: 200,
+    credentials: true,
+}
+
+app.use(cors(coreOptions));
+app.use(express.json({limit : '200kb'}));
+app.use(express.urlencoded({extended : true, limit : '200kb'}));
+app.use(express.static('public'));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+
+
 
 export default app;
