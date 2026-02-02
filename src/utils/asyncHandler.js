2a315c5cc0b5ca392301b0c2f24b2923f2849fce
Vikash <vikashyadav5131@gmail.com>
2026-02-02 17:02:14 +0530

    update asynchandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,7 @@
 const asynHandler = (requestHandler) =>{
     return (req, res, next) =>{
         Promise.resolve(requestHandler(req, res, next))
-        .catch((err) => {
-            if (typeof next === 'function') {
-                next(err);
-            } else {
-                throw err;
-            }
-        });
+        .catch((err) => next(err));
     }
 }
 
