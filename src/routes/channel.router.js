4d0dca92f27a5873d0d4e29933d4cfb646f1755b
Vikash <vikashyadav5131@gmail.com>
2026-02-09 19:19:20 +0530

    update the router of channel

--- src/routes/channel.router.js
+++ src/routes/channel.router.js
@@ -3,15 +3,16 @@ import verifyJWT from '../middlewares/auth.middlewares.js';
 import verifyChannelOwner from '../middlewares/channel.middleware.js';
 
 import {
+  createChannel,
   changeChannelName,
   changeDescription,
-  findSubscribers,
 } from '../controllers/channel.controllers.js';
 
 const channelRouter = express.Router();    
 
+channelRouter.post('/', verifyJWT, createChannel);
 channelRouter.patch('/name', verifyJWT, verifyChannelOwner, changeChannelName);
 channelRouter.patch('/description', verifyJWT, verifyChannelOwner, changeDescription);
-channelRouter.get('/subscribers', verifyJWT, verifyChannelOwner, findSubscribers);
+
 
 export default channelRouter;
